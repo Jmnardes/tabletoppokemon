56aae9c54688aa61160ea24afec60b8c5cb92452
Joao Marcus Ribeiro Nardes <joao.nardes@compasso.com.br>
2022-12-22 17:29:45 -0400

    feat: new balls

--- src/components/Pokemon/Treiner/PokeballStats.jsx
+++ src/components/Pokemon/Treiner/PokeballStats.jsx
@@ -2,18 +2,22 @@ import { Flex, Image, Text } from "@chakra-ui/react";
 import greatballIcon from '../../../assets/images/pokeballs/poke.png'
 import superballIcon from '../../../assets/images/pokeballs/golden.png'
 import ultraballIcon from '../../../assets/images/pokeballs/ultra.png'
+import premierballIcon from '../../../assets/images/pokeballs/premier.png'
+import masterballIcon from '../../../assets/images/pokeballs/master.png'
 
 export default function PokeballStats({
     greatball,
     superball,
-    ultraball
+    ultraball,
+    premierball,
+    masterball
 }) {
     return (
         <>
             <Flex alignItems="center" mx={3}>
                 <Image
                     src={greatballIcon} 
-                    alt={'greatball'}
+                    title={'+2'}
                     w="28px"
                 ></Image>
                 <Text fontSize="14px" mt={1} ml={0.5}>{greatball}</Text>
@@ -21,7 +25,7 @@ export default function PokeballStats({
             <Flex alignItems="center" mx={3}>
                 <Image
                     src={superballIcon} 
-                    alt={'superball'}
+                    title={'+3'}
                     w="28px"
                 ></Image>
                 <Text fontSize="14px" mt={1} ml={0.5}>{superball}</Text>
@@ -29,10 +33,26 @@ export default function PokeballStats({
             <Flex alignItems="center" mx={3}>
                 <Image
                     src={ultraballIcon} 
-                    alt={'ultraball'}
+                    title={'+5'}
                     w="28px"
                 ></Image>
                 <Text fontSize="14px" mt={1} ml={0.5}>{ultraball}</Text>
+            </Flex>
+            <Flex alignItems="center" mx={3}>
+                <Image
+                    src={premierballIcon} 
+                    title={'+7'}
+                    w="28px"
+                ></Image>
+                <Text fontSize="14px" mt={1} ml={0.5}>{premierball}</Text>
+            </Flex>
+            <Flex alignItems="center" mx={3}>
+                <Image
+                    src={masterballIcon} 
+                    title={'+10'}
+                    w="28px"
+                ></Image>
+                <Text fontSize="14px" mt={1} ml={0.5}>{masterball}</Text>
             </Flex>
         </>
     )

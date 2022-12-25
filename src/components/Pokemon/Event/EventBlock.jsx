9c14478748beb09075eb0758d54b81706c215996
Joao Marcus Ribeiro Nardes <joao.nardes@compasso.com.br>
2022-12-25 18:01:11 -0400

    feat: finish events

--- src/components/Pokemon/Event/EventBlock.jsx
+++ src/components/Pokemon/Event/EventBlock.jsx
@@ -8,31 +8,30 @@ export default function EventBlock({ disable }) {
     const [event, setEvent] = useState([])
 
     const handleEvent = () => {
-        rollEventType()
-
         setEvent(rollEventType())
     }
 
-    const EventComponent = ({ title, description, rules, prize1, prize2 }) => {
+    const EventComponent = ({ title, description, rules, first, second, third }) => {
         return (
             <Center flexDirection="column">
                 <Heading mb={8}>{title}</Heading>
                 <Text m={4} fontSize="2xl">{description}</Text>
                 <Text mt={8} fontSize="1xl" color="red" fontWeight="bold">{rules}</Text>
                 <Center mt={12} flexDirection="column">
-                    <Center mb={8}>
+                    <Center mb={6}>
                         <Text mr={4} fontSize="2xl" fontWeight="bold">1st</Text>
-                        <Text>{prize1}</Text>
+                        <Text>{first}</Text>
                     </Center>
-                    <Center mb={8}>
+                    <Center mb={6}>
                         <Text mr={4} fontSize="2xl" fontWeight="bold">2nd</Text>
-                        <Text>{prize2}</Text>
+                        <Text>{second}</Text>
                     </Center>
-                    <Center mb={8}>
+                    <Center mb={6}>
                         <Text mr={4} fontSize="2xl" fontWeight="bold">3nd</Text>
-                        <Text>1 coin</Text>
+                        <Text>{third}</Text>
                     </Center>
                 </Center>
+                <Text mt={4} color="red" fontSize="xs">Use tier sum or tier of participant pokemon to break ties</Text>
             </Center>
         )
     }
@@ -54,8 +53,9 @@ export default function EventBlock({ disable }) {
                     title={event?.title}
                     description={event?.description}
                     rules={event?.rules}
-                    prize1={event?.prize1}
-                    prize2={event?.prize2}
+                    first={event?.first}
+                    second={event?.second}
+                    third={event?.third}
                 />
             </Center>
         </PokeModal>
--- src/components/Pokemon/Event/events.jsx
+++ src/components/Pokemon/Event/events.jsx
@@ -2,22 +2,211 @@ import { diceRoll } from "../../../util"
 
 export const rollEventType = () => {
     let eventRoll = diceRoll(100)
+    let profit = eventProfit()
 
-    if(eventRoll < 50) {
+    if(eventRoll < 5) {
         return {
             title: 'Single Race',
-            description: 'Choose the highest speed you have!',
+            description: 'Use the highest speed on your team',
             rules: 'Roll a d6 and sum the stat',
-            prize1: '5 coins',
-            prize2: '3 coins'
+            first: profit.first,
+            second: profit.second,
+            third: profit.third
+        }
+    } else if(eventRoll < 10) {
+        return {
+            title: 'Dodge ball',
+            description: 'Use all speeds or all defenses on your team',
+            rules: 'Roll a d20 and sum the stats',
+            first: profit.first,
+            second: profit.second,
+            third: profit.third
+        }
+    } else if(eventRoll < 15) {
+        return {
+            title: 'Goalkeeper',
+            description: 'Use the highest defense on your team',
+            rules: 'Roll a d6 and sum the stat',
+            first: profit.first,
+            second: profit.second,
+            third: profit.third
+        }
+    } else if(eventRoll < 20) {
+        return {
+            title: 'Arm werstler',
+            description: 'Use the highest attack on your team',
+            rules: 'Roll a d6 and sum the stat',
+            first: profit.first,
+            second: profit.second,
+            third: profit.third
+        }
+    } else if(eventRoll < 25) {
+        return {
+            title: 'Slap contest',
+            description: 'Use the highest health on your team',
+            rules: 'Roll a 2d6 and sum the stat',
+            first: profit.first,
+            second: profit.second,
+            third: profit.third
+        }
+    } else if(eventRoll < 30) {
+        return {
+            title: 'Marathon',
+            description: 'Use all healths on your team',
+            rules: 'Roll a d20 and sum the stats',
+            first: profit.first,
+            second: profit.second,
+            third: profit.third
+        }
+    } else if(eventRoll < 35) {
+        return {
+            title: 'Tug of war',
+            description: 'Use all attacks on your team',
+            rules: 'Roll a d20 and sum the stats',
+            first: profit.first,
+            second: profit.second,
+            third: profit.third
+        }
+    } else if(eventRoll < 40) {
+        return {
+            title: 'Telecnesis',
+            description: 'If you have a Ghost or a Psychic pokemon',
+            rules: 'Roll a d6 and sum +1 for each pokemon on your team',
+            first: profit.first,
+            second: profit.second,
+            third: profit.third
+        }
+    } else if(eventRoll < 45) {
+        return {
+            title: 'Flying',
+            description: 'If you have a pokemon who flyes',
+            rules: 'Roll a d6 and sum +1 for each pokemon on your team',
+            first: profit.first,
+            second: profit.second,
+            third: profit.third
+        }
+    } else if(eventRoll < 50) {
+        return {
+            title: 'Flying',
+            description: 'If you have a pokemon who flyes',
+            rules: 'Roll a d6 and sum +1 for each pokemon on your team',
+            first: profit.first,
+            second: profit.second,
+            third: profit.third
+        }
+    } else if(eventRoll < 55) {
+        return {
+            title: 'Swimming',
+            description: 'If you have a Water or Ice pokemon',
+            rules: 'Roll a d6 and sum +1 for each pokemon on your team',
+            first: profit.first,
+            second: profit.second,
+            third: profit.third
+        }
+    } else if(eventRoll < 60) {
+        return {
+            title: 'Trim a tree',
+            description: 'Everyone can play, but Grass and Bug pokemons have advatages',
+            rules: 'Roll a d6 and sum +2 for each pokemon on your team',
+            first: profit.first,
+            second: profit.second,
+            third: profit.third
+        }
+    } else if(eventRoll < 65) {
+        return {
+            title: 'Scary face',
+            description: 'Everyone can play, but Ghost and Dark pokemons have advatages',
+            rules: 'Roll a d6 and sum +2 for each pokemon on your team',
+            first: profit.first,
+            second: profit.second,
+            third: profit.third
+        }
+    } else if(eventRoll < 70) {
+        return {
+            title: 'Recharge',
+            description: 'If you have a Electric pokemon',
+            rules: 'Roll a d6 and sum +1 for each pokemon on your team',
+            first: profit.first,
+            second: profit.second,
+            third: profit.third
+        }
+    } else if(eventRoll < 75) {
+        return {
+            title: 'Cuteness',
+            description: 'Everyone can play, but Fairy pokemons have advatages',
+            rules: 'Roll a d6 and sum +3 for each pokemon on your team',
+            first: profit.first,
+            second: profit.second,
+            third: profit.third
+        }
+    } else if(eventRoll < 80) {
+        return {
+            title: 'Chemistry',
+            description: 'Everyone can play, but Poison pokemons have advatages',
+            rules: 'Roll a d6 and sum +2 for each pokemon on your team',
+            first: profit.first,
+            second: profit.second,
+            third: profit.third
+        }
+    } else if(eventRoll < 85) {
+        return {
+            title: 'Sculpture',
+            description: 'Everyone can play, but Ground and Rock pokemons have advatages',
+            rules: 'Roll a d6 and sum +2 for each pokemon on your team',
+            first: profit.first,
+            second: profit.second,
+            third: profit.third
+        }
+    } else if(eventRoll < 90) {
+        return {
+            title: 'Eating contest',
+            description: 'Everyone can play, but Normal and Fighter pokemons have advatages',
+            rules: 'Roll a d6 and sum +1 for each pokemon on your team',
+            first: profit.first,
+            second: profit.second,
+            third: profit.third
+        }
+    } else if(eventRoll < 95) {
+        return {
+            title: 'Cooking',
+            description: 'Everyone can play, but Dragon and Fire pokemons have advatages',
+            rules: 'Roll a d6 and sum +1 for each pokemon on your team',
+            first: profit.first,
+            second: profit.second,
+            third: profit.third
         }
     } else {
         return {
-            title: 'Arm werstler',
-            description: 'Use all speeds or all defenses',
-            rules: 'Roll a d20 and sum the stats',
-            prize1: '5 coins',
-            prize2: '3 coins'
+            title: 'Karate chop',
+            description: 'If you have a Fighter, Rock or Steel pokemon',
+            rules: 'Roll a d6 and sum +1 for each pokemon on your team',
+            first: profit.first,
+            second: profit.second,
+            third: profit.third
+        }
+    }
+}
+
+function eventProfit() {
+    let profitRoll = diceRoll(100)
+
+    if(profitRoll < 90) {
+        return {
+            first: `${diceRoll(11) + 5} coins`,
+            second: `${diceRoll(4) + 2} coins`,
+            third: '1 coins'
+        }
+    } if(profitRoll < 98) {
+        return {
+            first: 'a Poke Star',
+            second: `${diceRoll(6) + 3} coins`,
+            third: `${diceRoll(2) + 1} coins`
+        }
+    } else {
+        return {
+            first: 'a Poke Crown',
+            second: 'a Poke Star',
+            third: `${diceRoll(6) + 5} coins`
         }
     }
 }

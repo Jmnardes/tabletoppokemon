508ffeedc1e2c4982c25ed9f88ce99fd6f6ec854
Joao Marcus Ribeiro Nardes <joao.nardes@compasso.com.br>
2023-04-09 21:57:28 -0400

    feat: event roll values

--- src/components/Pokemon/Modal/EventModals/OpponentsResult.jsx
+++ src/components/Pokemon/Modal/EventModals/OpponentsResult.jsx
@@ -2,16 +2,24 @@ import { Table, TableContainer, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/reac
 import { useContext } from "react"
 import PlayerContext from "../../../../Contexts/PlayerContext"
 
-export default function OpponentsResult() {
+export default function OpponentsResult({ opponentsRoll }) {
     const { opponents } = useContext(PlayerContext)
 
     const OpponentRow = ({opponent}) => {
         return (
             <Tr>
                 <Td pt={1} pb={2}>{opponent.status.trainerName}</Td>
-                <Td pt={1} pb={2}>{opponent.result ? '?' : opponent.result}</Td>
+                <Td pt={1} pb={2}>{opponentRollMatch(opponent)}</Td>
             </Tr>
         )
+    }
+
+    const opponentRollMatch = (opponent) => {
+        return opponentsRoll.map(roll => {
+            if(roll.id === opponent.id) {
+                return roll.roll
+            }
+        })
     }
 
     return (

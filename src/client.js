21f19db2d41fabfaf035b7cf0fb6b39a8061062e
Joao Marcus Ribeiro Nardes <joao.nardes@compasso.com.br>
2024-05-18 15:51:23 -0400

    chore: server listenner

--- src/client.js
+++ src/client.js
@@ -2,6 +2,7 @@ const io = require("socket.io-client")
 
 const socket = io('https://tabletoppokemon-server.onrender.com')
 // const socket = io('http://localhost:3001')
+// const socket = io('http://localhost:3002') //test main local
 
 // socket.on('get-players', (data) => {
 //   data

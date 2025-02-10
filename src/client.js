85838ac2a039a12c7ca705a6816a7a86773e1397
jmnardes <jmnardes@hotmail.com>
2025-02-10 19:48:59 -0400

    fix: log env

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 const io = require("socket.io-client");
 
 const SERVER_URL = process.env.REACT_APP_SOCKET_SERVER;
-console.log(SERVER_URL)
+console.log(SERVER_URL);
 
 const socket = io(SERVER_URL);
 

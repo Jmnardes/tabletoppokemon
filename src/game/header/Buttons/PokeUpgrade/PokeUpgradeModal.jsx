9f7fcb765eebae9b6c6c9915fadacb72615d1228
jmnardes <jmnardes@hotmail.com>
2025-03-03 21:45:28 -0400

    feat: remove pokebox from upgrades

--- src/game/header/Buttons/PokeUpgrade/PokeUpgradeModal.jsx
+++ src/game/header/Buttons/PokeUpgrade/PokeUpgradeModal.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react"
+import { useContext, useState } from "react"
 import PlayerContext from "@Contexts/PlayerContext"
 import { Text } from "@chakra-ui/react"
 import PokeList from "@components/Pokemon/PokeList"
@@ -6,27 +6,8 @@ import PokeUpgrade from "./PokeUpgrade"
 import GenericModal from "@components/Modal/GenericModal"
 
 export default function PokeUpgradeModal() {
-    const { updateGame, pokeBox, pokeTeam } = useContext(PlayerContext)
-    const [ allPokemon, setAllPokemon ] = useState([ ...pokeTeam, ...pokeBox ])
+    const { updateGame, pokeTeam } = useContext(PlayerContext)
     const [ selectedPokemon, setSelectedPokemon ] = useState(null)
-
-    useEffect(() => {
-        setAllPokemon(() => {
-            const newAllTeam = [ ...pokeTeam, ...pokeBox ]
-
-            if (selectedPokemon) {
-                const updatedSelectedPokemon = newAllTeam.find(
-                  poke => poke.id === selectedPokemon.id
-                )
-
-                if (updatedSelectedPokemon) {
-                  setSelectedPokemon(updatedSelectedPokemon)
-                }
-            }
-
-            return newAllTeam
-        })
-    }, [pokeBox, pokeTeam])
 
     return (
         <GenericModal 
@@ -35,7 +16,7 @@ export default function PokeUpgradeModal() {
             onModalClose={() => updateGame({ openPokeUpgradeModal: false })}
         >
             <Text fontSize={"small"} textAlign={"center"} mt={4}>Select the pokemon you wanna use your item</Text>
-            <PokeList allPokemon={allPokemon} setSelectedPokemon={setSelectedPokemon} />
+            <PokeList allPokemon={pokeTeam} setSelectedPokemon={setSelectedPokemon} />
 
             <PokeUpgrade selectedPokemon={selectedPokemon} setSelectedPokemon={setSelectedPokemon} />
         </GenericModal>

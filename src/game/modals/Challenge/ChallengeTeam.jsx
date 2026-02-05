fc91e9eb5980f00287dbe93d936c9ac0853cc400
944172 <joao.nardes@compasso.com.br>
2026-02-05 17:13:11 -0400

    feat: remove bonus logic from front

--- src/game/modals/Challenge/ChallengeTeam.jsx
+++ src/game/modals/Challenge/ChallengeTeam.jsx
@@ -4,7 +4,6 @@ import socket from "@client"
 import { Button, Center, Text } from "@chakra-ui/react";
 import ChallengeInfo from "./ChallengeInfo";
 import ChallengePokemonChoice from "./ChallengePokemonChoice";
-import { pokemonHasChallengeBerry } from "@utils";
 
 export default function ChallengeTeam({ event, bonus, setBonus, setTeamReady }) {
     const { teamIds, boxIds, pokemonData, player, session, emit } = useContext(PlayerContext)
@@ -13,35 +12,6 @@ export default function ChallengeTeam({ event, bonus, setBonus, setTeamReady }) 
 
     const allAvailableIds = [...teamIds, ...boxIds]
     const availablePokemons = allAvailableIds.map(id => pokemonData[id]).filter(Boolean)
-    const selectedPokemons = selectedPokemonIds.map(id => pokemonData[id]).filter(Boolean)
-
-    const checkChallengeBonus = (team) => {
-        if (!player?.status) return
-
-        setBonus(() => {
-            const augmentBonus = player.status.challengeBonus || 0;
-            
-            const generalBonuses =
-            team?.reduce((acc, poke) => {
-                if (event.advantage.type !== "element") return acc;
-
-                const types = poke.types ?? [];
-                const challengeBonus = pokemonHasChallengeBerry(poke) ? 1 : 0;
-
-                const perType = types.reduce((sum, element) => {
-                const isAdv = event.advantage.value.includes(element);
-                const isDis = event.disadvantage?.value?.includes(element);
-
-                const base = isAdv ? 1 : isDis ? -1 : 0;
-                return sum + base + challengeBonus;
-                }, 0);
-
-                return acc + perType;
-            }, 0) ?? 0;
-
-            return generalBonuses + (team?.length ?? 0) + augmentBonus;
-        });
-    };
 
     useEffect(() => {
         socket.on('event-challenge-start', res => {
@@ -53,17 +23,28 @@ export default function ChallengeTeam({ event, bonus, setBonus, setTeamReady }) 
             }
         })
 
+        socket.on('event-challenge-bonus-preview', res => {
+            if (res.bonus !== undefined) {
+                setBonus(res.bonus)
+            }
+        })
+
         return () => {
             socket.off('event-challenge-start')
+            socket.off('event-challenge-bonus-preview')
         }
         
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     useEffect(() => {
-        checkChallengeBonus(selectedPokemons)
+        if (selectedPokemonIds.length > 0 && player?.id && session?.sessionCode) {
+            emit('event-challenge-check-bonus', { pokemonIds: selectedPokemonIds }).catch(() => {})
+        } else if (selectedPokemonIds.length === 0) {
+            setBonus(0)
+        }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [selectedPokemonIds, pokemonData])
+    }, [selectedPokemonIds])
 
     const handleTogglePokemon = (pokemonId) => {
         setSelectedPokemonIds(prev => {
